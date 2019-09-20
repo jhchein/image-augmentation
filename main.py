b994f3bde49f6d00fc8510940b7968ac582ae7ab
Hendrik Hein <hehein@microsoft.com>
2019-09-20 17:37:41 +0200

    removed print statements

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ os.makedirs(output_path, exist_ok=True)
 
 origin_images = [f for f in glob.glob(f"{path}/*{origin_filetype}", recursive=True)]
 
-print(path)
-print(origin_images)
-
 for full_filename in origin_images:
     print(full_filename)
     augmenter = Data_augmentation(full_filename)
     output_filename = augmenter.image_augment(save_path=output_path)
+
+print("done")
